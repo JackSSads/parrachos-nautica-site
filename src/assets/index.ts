6e82641fd11c973d7a106b29af8e67c840221358
JackSSads <contatojacksonsouza@hotmail.com>
2025-12-13 15:35:40 -0300

    exportação padrão para imagens

---
+++ src/assets/index.ts
@@ -0,0 +1,69 @@
+import Catamara01 from "@/assets/catamara/catamara.webp";
+import Catamara02 from "@/assets/catamara/catamara02.webp";
+
+import Parrachos01 from "@/assets/parrachos/parrachos.webp";
+import Parrachos02 from "@/assets/parrachos/f_aerea.webp";
+import Parrachos03 from "@/assets/parrachos/f_aerea02.webp";
+
+import Snorkel01 from "@/assets/snorkel/snorkel01.webp";
+import Snorkel02 from "@/assets/snorkel/snorkel02.webp";
+import Snorkel03 from "@/assets/snorkel/snorkel03.webp";
+
+import Cilindro01 from "@/assets/cilindro/cilindro01.webp"
+import Cilindro02 from "@/assets/cilindro/cilindro02.webp"
+import Cilindro03 from "@/assets/cilindro/cilindro03.webp"
+import Cilindro04 from "@/assets/cilindro/cilindro04.webp"
+import Cilindro05 from "@/assets/cilindro/cilindro05.webp"
+import Cilindro06 from "@/assets/cilindro/cilindro06.webp"
+import Cilindro07 from "@/assets/cilindro/cilindro07.webp"
+import Cilindro08 from "@/assets/cilindro/cilindro08.webp"
+
+import Quadriciclo02 from "@/assets/quadriciclo/quadriciclo02.webp";
+import Quadriciclo03 from "@/assets/quadriciclo/quadriciclo03.webp";
+import Quadriciclo04 from "@/assets/quadriciclo/quadriciclo04.webp";
+import Quadriciclo05 from "@/assets/quadriciclo/quadriciclo05.webp";
+import Quadriciclo08 from "@/assets/quadriciclo/quadriciclo08.webp";
+import Quadriciclo09 from "@/assets/quadriciclo/quadriciclo09.webp";
+import Quadriciclo10 from "@/assets/quadriciclo/quadriciclo10.webp";
+import Quadriciclo11 from "@/assets/quadriciclo/quadriciclo11.webp";
+
+const catamara = {
+    Catamara01,
+    Catamara02
+};
+
+const parrachos = {
+    Parrachos01,
+    Parrachos02,
+    Parrachos03,
+};
+
+const snorkel = {
+    Snorkel01,
+    Snorkel02,
+    Snorkel03,
+};
+
+const cilindro = {
+    Cilindro01,
+    Cilindro02,
+    Cilindro03,
+    Cilindro04,
+    Cilindro05,
+    Cilindro06,
+    Cilindro07,
+    Cilindro08,
+};
+
+const quadriciclo = {
+    Quadriciclo02,
+    Quadriciclo03,
+    Quadriciclo04,
+    Quadriciclo05,
+    Quadriciclo08,
+    Quadriciclo09,
+    Quadriciclo10,
+    Quadriciclo11,
+}
+
+export { catamara, parrachos, snorkel, cilindro, quadriciclo }
